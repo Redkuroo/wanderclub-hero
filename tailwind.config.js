db26dd7ed828d1313e1802b1e8b2fc0bf5bf8d26
John Smile Mella <02jsmella@gmail.com>
2025-07-14 14:14:55 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['TT Norms Pro', 'Arial', 'Helvetica', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}; 
